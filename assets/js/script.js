b444308a8740d6b236cc1e20a8e12bf9dfa24b0b
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-31 20:34:11 -0400

    script.js had the search button function outline created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+function searchButtonClicked() {
+    var searchInput = document.getElementById('searchInput');
+    var searchValue = searchInput.value;
+    if (searchValue) {
+        window.location.href = '/search/' + searchValue;
+    }
+    else {
+        alert('Please enter a search term');
+    }
+}
+function searchInputKeyPressed(event) {
+    if (event.key === 'Enter') {
+        searchButtonClicked();
+    }
+}
 
