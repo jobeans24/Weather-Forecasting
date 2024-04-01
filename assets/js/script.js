d05d88bb6067aa8f93b94372949b5ab1aa8d5451
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-31 20:28:59 -0400

    creating the jquery and making the fetch function to receive the information the user is requesting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,1 @@
-function getCoordinates() {
-    var geocoder = new google.maps.Geocoder();
-    var address = "New York, NY"; // Replace with the desired location
 
-    geocoder.geocode({ 'address': address }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            var latitude = results[0].geometry.location.lat();
-            var longitude = results[0].geometry.location.lng();
-            console.log("Latitude: " + latitude);
-            console.log("Longitude: " + longitude);
-        } else {
-            console.log("Geocode was not successful for the following reason: " + status);
-        }
-    });
-}
